3984a2906de941a47cba8d50cb24d31bb385ab09
yeonise <cerealnum@gmail.com>
2024-01-06 16:24:04 +0900

    binary search 관련 설명 추가

--- implementation/binary search.py
+++ implementation/binary search.py
@@ -24,10 +24,17 @@ def upper_bound(arr, target):
     return low
 
 
-# 예시 배열
-arr = [1, 2, 4, 4, 4, 6, 7]
+# 이분 탐색이 원하는 값 k를 찾는 과정이라면
+# Lower Bound는 원하는 값 k 이상이 처음 등장하는 위치를 찾는 과정이고
+# Upper Bound는 원하는 값 k를 초과한 값이 처음 나오는 위치를 찾는 과정이다
+
+# Lower Bound는 정렬된 배열에서 탐색값이 2개 이상 있는 경우 가장 앞에 위치한 탐색값을 의미한다
+# ex. 1 - [2] - 2 - 2 - 2 - 2 - 8 - 9
+# Upper Bound는 가장 뒤에 위치한 탐색값의 다음 위치를 의미한다
+# ex. 1 - 2 - 2 - 2 - 2 - 2 - [8] - 9
 
 # Lower Bound와 Upper Bound 예시
+arr = [1, 2, 4, 4, 4, 6, 7]
 target_value = 4
 lower_bound_index = lower_bound(arr, target_value)
 upper_bound_index = upper_bound(arr, target_value)
