067746e9340a9ed17d4028b05a11c28b75f201d5
yeonise <cerealnum@gmail.com>
2024-01-24 10:44:29 +0900

    programmers / level 3 / 야근 지수 - 코드 개선

--- programmers/Level3/야근 지수.py
+++ programmers/Level3/야근 지수.py
@@ -1,30 +1,15 @@
-from collections import Counter
 from heapq import heapify, heappop, heappush
 
 
 def solution(n, works):
-    counter = Counter(works)
-    works = list(map(lambda x: x * - 1, works))
+    if n >= sum(works):
+        return 0
+
+    works = [-work for work in works]
     heapify(works)
-    finished = 0
 
-    while works and finished < n:
-        current_work = heappop(works) * -1
-        if counter[current_work] > 0:
-            counter[current_work] -= 1
+    for _ in range(n):
+        work = heappop(works)
+        heappush(works, work + 1)
 
-            updated_work = current_work - 1
-            if updated_work > 0:
-                if updated_work in counter:
-                    counter[updated_work] += 1
-                else:
-                    counter[updated_work] = 1
-                heappush(works, updated_work * -1)
-
-            finished += 1
-
-    answer = 0
-    for key, value in counter.items():
-        answer += (key ** 2) * value
-
-    return answer
+    return sum([work ** 2 for work in works])
